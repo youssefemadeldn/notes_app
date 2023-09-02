4dcaf90dd2862bf8d66f0e17464ae34ab3cfdb8f
youssef emad eldin <youssefemad63.ye@gmail.com>
2023-09-02 15:17:23 +0300

    create notes app

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const NotesApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
 
   // This widget is the root of your application.
   @override
