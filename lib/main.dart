11ebff7c0277a6d748c13402d4e5d107da95e407
youssef emad eldin <youssefemad63.ye@gmail.com>
2023-09-02 16:08:46 +0300

    add photo and change theme and create NotesViwe

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:noteapp/views/notes_view.dart';
 
 void main() {
   runApp(const NotesApp());
@@ -10,6 +11,10 @@ class NotesApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: const NotesView(),
+    );
   }
 }
