e35b7da04b9d58c51e31d42a54fb0e51ed7e6b39
youssef emad eldin <youssefemad63.ye@gmail.com>
2023-09-03 22:05:48 +0300

    create note model

---
+++ models/note_model.dart
@@ -0,0 +1,12 @@
+class NoteModel {
+  final String titel;
+  final String subTitel;
+  final String date;
+  final int color;
+
+  NoteModel(
+      {required this.titel,
+      required this.subTitel,
+      required this.date,
+      required this.color});
+}
