8a67eeaedd719c83e1faa666828f7248c695c81a
youssef emad eldin <youssefemad63.ye@gmail.com>
2023-09-03 12:35:35 +0300

    show model bottom sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:noteapp/widgets/add_note_bottom_sheet.dart';
 import 'package:noteapp/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -8,10 +9,20 @@ class NotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return const AddNoteBottumSheet();
+              });
+        },
         child: const Icon(Icons.add),
       ),
       body: const NotesViewBody(),
     );
   }
 }
+
+// AddNoteBottumSheet()
+
+
--- lib/widgets/note_list_view.dart
+++ lib/widgets/note_list_view.dart
@@ -7,12 +7,22 @@ class NotesListViwe extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: ListView.builder(itemBuilder: (context, index) {
-        return const Padding(
-          padding: EdgeInsets.symmetric(vertical: 8),
-          child: NoteItem(),
-        );
-      }),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16),
+        child: ListView.builder(itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.zero,
+            child: Column(
+              children: [
+                NoteItem(),
+                SizedBox(
+                  height: 10,
+                )
+              ],
+            ),
+          );
+        }),
+      ),
     );
   }
 }
