52b7bf26ae5b1ea83683aebf664bfee442f6f681
youssef emad eldin <youssefemad63.ye@gmail.com>
2023-09-03 18:13:05 +0300

    create edite view note

---
+++ lib/views/edite_note_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:noteapp/widgets/edite_view_body.dart';
+
+class EditeNoteView extends StatelessWidget {
+  const EditeNoteView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: EditeNoteViewBody(),
+    );
+  }
+}
---
+++ lib/widgets/edite_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EditeNoteViewBody extends StatelessWidget {
+  const EditeNoteViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
