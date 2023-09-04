54818dd164312b20962e05b9fc41d9ba57fab461
youssef emad eldin <youssefemad63.ye@gmail.com>
2023-09-04 15:39:56 +0300

    create typr adapter fot note

--- models/note_model.dart
+++ models/note_model.dart
@@ -1,12 +1,20 @@
-class NoteModel {
-  final String titel;
-  final String subTitel;
+import 'package:hive/hive.dart';
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
+  final String title;
+  @HiveField(1)
+  final String subTitle;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
 
   NoteModel(
-      {required this.titel,
-      required this.subTitel,
+      {required this.title,
+      required this.subTitle,
       required this.date,
       required this.color});
 }
