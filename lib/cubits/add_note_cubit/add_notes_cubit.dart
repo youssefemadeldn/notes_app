591701389430cc29018f3d9720b93c22893ecaef
youssef emad eldin <youssefemad63.ye@gmail.com>
2023-09-05 10:15:40 +0300

    add note methode to add note cubit

--- lib/cubits/add_note_cubit/add_notes_cubit.dart
+++ lib/cubits/add_note_cubit/add_notes_cubit.dart
@@ -1,7 +1,12 @@
+//import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:noteapp/models/note_model.dart';
+
 part 'add_notes_state.dart';
 
-class NotesCubit extends Cubit<NotesState> {
-  NotesCubit() : super(NotesInitial());
+class AddNotesCubit extends Cubit<AddNoteState> {
+  AddNotesCubit() : super(AddNoteInitial());
+
+  addNote(NoteModel note) {}
 }
--- lib/cubits/add_note_cubit/add_notes_state.dart
+++ lib/cubits/add_note_cubit/add_notes_state.dart
@@ -1,6 +1,16 @@
 part of 'add_notes_cubit.dart';
 
 @immutable
-abstract class NotesState {}
+abstract class AddNoteState {}
 
-class NotesInitial extends NotesState {}
+class AddNoteInitial extends AddNoteState {}
+
+class AddNoteLoading extends AddNoteState {}
+
+class AddNoteSucscss extends AddNoteState {}
+
+class AddNoteFailur extends AddNoteState {
+  final String errMessage;
+
+  AddNoteFailur(this.errMessage);
+}
